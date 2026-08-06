bccd39f49ce56b3c2620053129a5d821e2c0bb4c
sutha07 <149075514+sutha07@users.noreply.github.com>
2026-08-06 14:25:25 +0530

    Add production CORS origin for thisutech

--- server/thisutech/settings.py
+++ server/thisutech/settings.py
@@ -117,6 +117,6 @@ SIMPLE_JWT = {
 # CORS
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:5173,http://127.0.0.1:5173'
+    default='http://localhost:5173,http://127.0.0.1:5173,https://thisutech.onrender.com/'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
